4a05c854eb85e7db749677267b9d7a401b4edde0
vengateshp <vengatcse1997@gmail.com>
2016-12-23 14:40:21 +0530

    Add files via upload

---
+++ program1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("Enter the number:");
+scanf("%d",&a);
+if(a%2==0)
+printf("Given number is even number");
+else
+printf("Given number is odd number");
+getch();
+}
