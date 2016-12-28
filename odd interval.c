cfb71461f580dccbea60190af7c164b7c98159ef
vengateshp <vengatcse1997@gmail.com>
2016-12-28 10:09:34 +0530

    Add files via upload

---
+++ odd interval.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int i,j;
+   printf("Enter the two intervels:");
+   scanf("%d %d",&i,&j);
+   for(i=i+1;i<j;i++)
+    if(i%2!=0)
+    printf("%d",i);
+
+}
