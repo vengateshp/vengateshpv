110bc593a900b6d11eae73e4919595a92d5f17ad
vengateshp <vengatcse1997@gmail.com>
2016-12-28 19:44:07 +0530

    Add files via upload

---
+++ reverse.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+   char a[100];
+
+   printf("Enter a string to reverse\n");
+   gets(a);
+
+   strrev(a);
+
+   printf("Reverse of entered string is \n%s\n",a);
+
+   return 0;
+}
