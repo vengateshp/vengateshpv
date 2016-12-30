9f8386d39179c1e3701516efb884a00e5d647dc7
vengateshp <vengatcse1997@gmail.com>
2016-12-30 10:02:37 +0530

    Add files via upload

---
+++ reverse of integer.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int n, a=0;
+    printf("Enter a number to reverse\n");
+    scanf("%d",&n);
+
+   while(n!=0)
+    {
+        a = a * 10;
+
+        a = a + n%10;
+
+        n = n/10;
+    }
+
+    printf("Reverse of entered number is = %d\n", a);
+    return 0;
+}
